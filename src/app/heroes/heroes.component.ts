74c54ddec1456ca7401710187cfe05e0d37c7b02
yonisGit <yoniel@post.bgu.ac.il>
2020-03-17 18:07:53 +0200

    finished step5!

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,7 +1,7 @@
-import {Component, OnInit} from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+import { HeroService } from '../hero.service';
 import {Hero} from '../entities/hero';
-import {HeroService} from '../hero.service';
-import {MessageService} from '../message.service';
 
 @Component({
   selector: 'app-heroes',
@@ -9,28 +9,16 @@ import {MessageService} from '../message.service';
   styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent implements OnInit {
-
-  selectedHero: Hero;
   heroes: Hero[];
 
-  onSelect(hero: Hero): void {
-    this.selectedHero = hero;
-    this.messageService.add(`HeroService: Selected hero id=${hero.id}`);
-  }
+  constructor(private heroService: HeroService) { }
 
-  constructor(private heroService: HeroService, private messageService: MessageService) {
-  }
-
-  ngOnInit(): void {
+  ngOnInit() {
     this.getHeroes();
   }
 
   getHeroes(): void {
-    this.heroService
-      .getHeroes()
-      .subscribe(heroes => {
-        this.heroes = heroes;
-      });
+    this.heroService.getHeroes()
+      .subscribe(heroes => this.heroes = heroes);
   }
-
 }
