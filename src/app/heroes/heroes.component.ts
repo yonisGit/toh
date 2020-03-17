e182b48eb0b0bd7b16c4b99c51708e68eabd81cf
yonisGit <yoniel@post.bgu.ac.il>
2020-03-17 12:07:21 +0200

    added a list of heroes.

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import {Hero} from '../entities/hero';
+import {HEROES} from '../entities/mock-heroes';
 
 @Component({
   selector: 'app-heroes',
@@ -8,6 +9,7 @@ import {Hero} from '../entities/hero';
 })
 export class HeroesComponent implements OnInit {
 
+  heroes = HEROES;
   hero: Hero = {
   id: 1,
   name: 'Thor'
